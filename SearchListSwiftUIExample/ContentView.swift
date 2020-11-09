142ebe379ebd405784984eac1df3e62690fe6c40
John Codeos <johncodeos@gmail.com>
2020-11-09 16:06:54 +0200

    Initial Commit

---
+++ SearchListSwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchListSwiftUIExample
+//
+//  Created by John Codeos on 11/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchListSwiftUIExample/SearchListSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListSwiftUIExampleApp.swift
+//  SearchListSwiftUIExample
+//
+//  Created by John Codeos on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct SearchListSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
